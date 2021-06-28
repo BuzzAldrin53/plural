79c2cf05f349f983c887135b978fa6abe9b3044d
BuzzAldrin53 <86384404+BuzzAldrin53@users.noreply.github.com>
2021-06-28 09:08:00 -0500

    Add files via upload

---
+++ maqueta2/core_menu_top.php
@@ -0,0 +1,15 @@
+			<ul>
+
+			<li><a href="https://pluralmkt.mx/my-account/">CONTACTO</a></li>
+			<LI><a href="https://pluralmkt.mx/cart/">CARRITO</a></li>
+			<LI><a href="https://pluralmkt.mx/wishlist/">LISTA DE DESEOS</a></li>
+			<LI><a href="https://pluralmkt.mx/registrate/">REGÍSTRATE</a></li>
+			<li><a href="https://pluralmkt.mx/my-account/">MI CUENTA</a></li>
+			<li>Bienvenido</li>
+			
+
+			</ul>
+			
+			
+			
+			
---
+++ maqueta2/db_test.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+ $DB_USER='pluralmk_admin'; 
+ $DB_PASS='plur4l4dm1n'; 
+ $DB_HOST='127.0.0.1';
+ $DB_NAME='pluralmk_db';
+ 
+    extract($_POST);
+    extract($_GET); 
+ 
+
+    $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+    /* check connection */
+
+
+
+
+
+    $mysqli->query("SET NAMES 'utf8'");
+    $sql="
+
+INSERT INTO `reciente` (`id_reciente`, `categoria`, `url_categoria`, `nombre`, `estrellas`, `imagen`, `url`, `clicks`) VALUES (NULL, 'ddd', 'd', 'd', '2', 'dd', 'd', '2');
+
+    ";
+
+
+     $result=$mysqli->query($sql);
+
+  
+    echo '{"status":"true","message":"Data fetched successfully!","data":""}';
+  
+
+
+  
+  $mysqli->close(); 
+
+
+
+    ?>
